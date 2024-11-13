d0994d82d30e80416c6a9bcfd8fa2f31b2737da9
Ananthu T J <ananthutj832@gmail.com>
2024-11-13 23:34:30 +0530

    Changed the readme file after completing the search functionality

--- lib/presentations/screens/Home/home_screen.dart
+++ lib/presentations/screens/Home/home_screen.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 import '../../../data/models/user.dart';
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
